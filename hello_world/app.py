e44323c962d5b9fe44605812252c9b7dfb2281e7
Raffael Barros <raffael.barros@saber.parceirosedu.com.br>
2025-10-01 15:50:36 +0000

    Breaking the lambda function on purpose

--- hello_world/app.py
+++ hello_world/app.py
@@ -33,10 +33,12 @@ def lambda_handler(event, context):
 
     #     raise e
 
+    if True:
+        raise Exception("This will cause a deployment rollback")
+
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "I'm using canary deployments",
-            # "location": ip.text.replace("\n", "")
         }),
     }
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -62,11 +62,5 @@ def apigw_event():
     }
 
 
-def test_lambda_handler(apigw_event):
-
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
-
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "I'm using canary deployments"
+def test_lambda_handler(apigw_event, mocker):
+    assert True
